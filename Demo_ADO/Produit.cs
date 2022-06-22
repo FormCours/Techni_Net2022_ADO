acabc42e86c6f2f5717ad880d8ecb518be1facfa
Aurelien <aurelien.strimelle@bstorm.be>
2022-06-22 17:10:36 +0200

    Ajoutez des fichiers projet.

---
+++ Demo_ADO/Produit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_ADO
+{
+    internal class Produit
+    {
+        public int  Id { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+    }
+}
---
+++ Demo_ADO/Program.cs
@@ -0,0 +1,32 @@
+﻿using Demo_ADO;
+using System.Data.SqlClient;
+
+string connectionString = "Server=TFNSDOTDE0400B; Database=Exo02; User Id=StudentAdmin; Password=Test1234=;";
+
+// Créer la connection
+SqlConnection connection = new SqlConnection(connectionString);
+
+// Définir la commande SQL
+SqlCommand cmd = connection.CreateCommand();
+cmd.CommandText = "SELECT * FROM Produit";
+cmd.CommandType = System.Data.CommandType.Text;
+
+// Ouvrir la connection
+connection.Open();
+
+// Executer la commande SQL
+SqlDataReader reader = cmd.ExecuteReader();
+
+while(reader.Read())
+{
+    Produit p = new Produit
+    {
+        Id = (int)reader["ID"],
+        Name = (string)reader["Nom"],
+        Desc = (string)reader["Description"]
+    };
+    Console.WriteLine($"{p.Id} / {p.Name} / {p.Desc}");
+}
+
+// Fermeture la connection
+connection.Close();
